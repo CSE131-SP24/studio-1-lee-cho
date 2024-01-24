bb9b98131e839fb84fca1b6d641db9a14a8a7c97
aileen <aileen@aileens-mbp.dhcp.wustl.edu>
2024-01-24 15:26:07 -0600

    Finished

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ordered {
 
 	/**
-	 * @param args
+	 boolean isOrdered = true;
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -15,7 +15,8 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered= x<y && y<z || x>y && y>z;
+		System.out.println(isOrdered);
 	}
 
 }
